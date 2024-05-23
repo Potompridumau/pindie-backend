08264396fdcdb67e2c204d8d198699bb705d5de4
Potompridumau <vfvfhsitv@gmail.com>
2024-05-23 09:11:09 +0300

    cors

--- middlewares/cors/cors.js
+++ middlewares/cors/cors.js
@@ -1,26 +1,32 @@
 require('dotenv').config();
 
 const ALLOWEDCORS = [
-	'https://practicum.yandex.ru',
-	'https://students-projects.ru',
-	'http://localhost:3000',
-	'http://localhost:3001',
-	'https://rich-frontend-practicum.nomoredomainswork.ru',
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://rich-frontend-practicum.nomoredomainswork.ru',
 ];
 
 function cors(req, res, next) {
-	const { origin } = req.headers;
+    const { origin } = req.headers;
 
-	if (ALLOWEDCORS.includes(origin)) {
-		res.header('Access-Control-Allow-Origin' || 'application/json', origin);
-		res.header('Access-Control-Allow-Credentials', true);
-		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-		res.header(
-			'Access-Control-Allow-Headers',
-			'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
-		);
-	}
-	next();
+    // Проверяем, разрешен ли исходный запрос
+    if (ALLOWEDCORS.includes(origin)) {
+        // Устанавливаем заголовок Access-Control-Allow-Origin
+        res.setHeader('Access-Control-Allow-Origin', origin);
+
+        // Разрешаем использование учетных данных
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+        // Разрешаем заголовки
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
+
+        // Разрешаем методы
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
+
+    next();
 }
+
 module.exports = cors;
